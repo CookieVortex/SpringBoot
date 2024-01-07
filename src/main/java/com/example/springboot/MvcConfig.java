f52b4ea2153166fb6aea217372b2bd2ed0406bb7
Maxim <golub94@mail.ru>
2024-01-08 00:30:16 +0200

    Initial commit

--- src/main/java/com/example/springboot/MvcConfig.java
+++ src/main/java/com/example/springboot/MvcConfig.java
@@ -1,6 +1,8 @@
 package com.example.springboot;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
