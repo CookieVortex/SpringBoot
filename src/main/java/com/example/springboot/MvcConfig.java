53ea8b3efac6f58657a33bc96e51e7f959e21282
Maxim Golubev <golub94@mail.ru>
2024-01-08 00:35:20 +0200

    Add files via upload

---
+++ src/main/java/com/example/springboot/MvcConfig.java
@@ -0,0 +1,19 @@
+package com.example.springboot;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
---
+++ src/main/java/com/example/springboot/UserLogController.java
@@ -0,0 +1,34 @@
+package com.example.springboot;
+
+import com.example.springboot.entity.UserLog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class UserLogController {
+
+    private final UserLogRepository userLogRepository;
+
+    @Autowired
+    public UserLogController(UserLogRepository userLogRepository) {
+        this.userLogRepository = userLogRepository;
+    }
+
+    @GetMapping("/")
+    public String getUserLog(Model model) {
+        List<UserLog> userLogs = userLogRepository.findAll();
+        model.addAttribute("data", userLogs);
+        return "home"; //
+    }
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        List<UserLog> userLogs = userLogRepository.findAll();
+        model.addAttribute("data", userLogs);
+        return "home"; //
+    }
+}
