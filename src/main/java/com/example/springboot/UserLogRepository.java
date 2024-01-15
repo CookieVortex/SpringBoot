f541cc742946289256cc6b296c565c33411e7642
Maxim Golubev <golub94@mail.ru>
2024-01-16 00:16:50 +0200

    Delete src directory

--- src/main/java/com/example/springboot/UserLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot;
-
-import com.example.springboot.entity.UserLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserLogRepository extends JpaRepository<UserLog, Long> {
-
-}
-
